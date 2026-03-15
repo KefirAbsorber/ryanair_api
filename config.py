318252019044868d4d2747d0b0bd98138509d911
KefirAbsorber <jan.bogdanowicz05@gmail.com>
2026-03-15 18:07:34 +0100

    Added headers to displaying info to improve clarity

--- config.py
+++ config.py
@@ -4,8 +4,20 @@ config = configparser.ConfigParser()
 # full request is: frm, to, date_from, date_to (date format yyyy-mm-dd)
 while True:
     config.read("settings.ini")
-    mode = input("Choose mode: \n1. Add tracking \n2. Delete tracking \n3. Update tracking \n4. Exit\n")
+    mode = input("Choose mode: \n0. Check tracking \n1. Add tracking \n2. Delete tracking \n3. Update tracking \n4. Exit\n")
     match mode:
+        case "0": #check
+            print("Entries in settings.ini:\n id frm  to  date_from  date_to")
+            queries = config.sections()
+
+            if not queries:
+                print("No entries found, nothing to display")
+                continue
+
+            for query in queries:
+                print(str(config[query])[9:], config[query]["frm"], config[query]["to"], config[query]["date_from"],
+                      config[query]["date_to"])
+
         case "1":  # add
             # grab next index or set to 1
             num = config.sections()
@@ -22,7 +34,7 @@ while True:
             config[num]["date_to"] = input("Enter ending date (in format yyyy-mm-dd): ")
 
         case "2":  # delete
-            print("Entries in settings.ini:")
+            print("Entries in settings.ini:\n id frm  to  date_from  date_to")
             queries = config.sections()
 
             if not queries:
